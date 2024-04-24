0f497af424d65435ab3b7eb417961f7296b417ad
Chethan <90902416+Ekaangi@users.noreply.github.com>
2024-04-24 12:31:07 +0530

    Update Face_recog_CustomKNN.py

--- Face_recog_CustomKNN.py
+++ Face_recog_CustomKNN.py
@@ -3,31 +3,25 @@ import numpy as np
 import os
 import math
 
-#-------------------------------KNN STARTS-----------------------------------------
-
-# Define distance function
+#KNN Algorithm
 def dist(x1, x2):
     distance = 0
     for i in range(len(x1)):
         distance += (x1[i] - x2[i]) ** 2
     return math.sqrt(distance)
-
-# Define KNN algorithm
+    
 def KNN(train, test, k=2):
-    # X will have all the features except the last one as the last one will contain the ID
     X_train = [row[:-1] for row in train]
     Y_train = [row[-1] for row in train]
-    # Creating a list to save the value of distance between test and training point and will save id also
     vals = []
 
     for i in range(len(X_train)):
         vals.append((dist(test, X_train[i]), Y_train[i]))
 
-    # Now we will sort the vals on the basis of distance and will take only first 'k' values from it as they
-    # will be the nearest ones
+    # sort the vals on the basis of distance , take only first 'k' values as they will be the nearest ones
     vals = sorted(vals)[:k]
 
-    # Now we will find which class id is more closer to the test image.
+    #finding which class id is more closer
     class_counts = {}
     for val in vals:
         if val[1] in class_counts:
@@ -35,7 +29,7 @@ def KNN(train, test, k=2):
         else:
             class_counts[val[1]] = 1
 
-    # Find the class with the highest count
+    # Finding the class with the highest count
     max_count = -1
     predicted_class = None
     for cls, count in class_counts.items():
@@ -44,23 +38,18 @@ def KNN(train, test, k=2):
             predicted_class = cls
 
     return(int(predicted_class))
-    
-#--------------------------------KNN ENDS----------------------------------------------------------
+
 
 face_data=[]
 
-#This will an array which will save class ids.
 labels=[]
 
-#defining class id which will allocated to every image.
 class_id=0
 
-#A dcitionary is defined here which will treat class id as key and its value will be the name of the file
 names={}
 
 for file in os.listdir():
     if file.endswith('.npy'):
-        #Loading the training data one at a time
         data_item=np.load(file)
         face_data.append(data_item)
         
@@ -68,7 +57,6 @@ for file in os.listdir():
         labels.append(target)
         
         names[class_id]=file[:-4]
-        #incrementing class id after each iteration
         class_id+=1
 
 face_data=np.concatenate(face_data,axis=0)
@@ -93,7 +81,6 @@ while True:
 
         face_section=face_section.flatten()
         
-        #Now calling the KNN to predict the output
         output=KNN(training_data,face_section,k=5)
         predicted_name=names[output]
         
